511852bd2c0a1ec741094d9d53cf1c0c5226d7ad
vidyasagar <vidyasagar.ponnam@digiscloud.com>
2019-05-06 22:57:13 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,9 +17,9 @@ const users=JSON.parse(userData);
 app.get('/',(req,res)=>res.render('index',{title:'Account Summary'},{accounts:accounts}));
 
 
-app.get('/savings',(req,res)=>res.render('account',{account:account.savings}));
-app.get('/checking',(req,res)=>res.render('account',{account:account.savings}));
-app.get('/credit',(req,res)=>res.render('account',{account:account.savings}));
+app.get('/savings',(req,res)=>res.render('account',{account:accounts.savings}));
+app.get('/checking',(req,res)=>res.render('account',{account:accounts.savings}));
+app.get('/credit',(req,res)=>res.render('account',{account:accounts.savings}));
 app.get('/profile',(req,res)=>res.render('profile',{user:users[0]}));
 app.listen(3000,()=>console.log("PS Project Running on port 3000!"));
 
