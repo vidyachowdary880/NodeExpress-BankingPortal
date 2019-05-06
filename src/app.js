3a4d19b28a9e89dd9bb78c8ccd57d2e0b0ea695e
vidyasagar <vidyasagar.ponnam@digiscloud.com>
2019-05-06 22:49:37 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -7,7 +7,19 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',(req,res)=>res.render('index',{title:'Index'}));
+const accountData=fs.readFileSync(path.join(__dirname,'json','accounts.json'), 'utf8');
+const accounts=JSON.parse(accountData);
 
+const userData=fs.readFileSync(path.join(__dirname,'json','users.json'), 'utf8');
+const users=JSON.parse(userData);
+
+ 
+app.get('/',(req,res)=>res.render('index',{title:'Account Summary'},{accounts:accounts}));
+
+
+app.get('/savings',(req,res)=>res.render('account',{account:account.savings}));
+app.get('/checking',(req,res)=>res.render('account',{account:account.savings}));
+app.get('/credit',(req,res)=>res.render('account',{account:account.savings}));
+app.get('/profile',(req,res)=>res.render('profile',{user:users[0]}));
 app.listen(3000,()=>console.log("PS Project Running on port 3000!"));
 
