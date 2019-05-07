eefe4a5992bdc63f7bd44c64710a2c024597c0aa
vidyasagar <vidyasagar.ponnam@digiscloud.com>
2019-05-07 22:08:56 +0530

    v

--- src/app.js
+++ src/app.js
@@ -23,16 +23,18 @@ app.get('/checking',(req,res)=>res.render('account',{account:accounts.savings}))
 app.get('/credit',(req,res)=>res.render('account',{account:accounts.savings}));
 app.get('/profile',(req,res)=>res.render('profile',{user:users[0]}));
 app.get('/transfer',(req,res)=>res.render('transfer'));
+
 app.post('/transfer',(req,res)=>{
 accounts[req.body.from].balance=accounts[req.body.form].balance-req.body.amount;
 accounts[req.body.to].balance=parseInt(accounts[req.body.to].balance)+parseInt(req.body.amount,10);
 const accountsJSON= JSON.stringify(accounts,null,4);
 fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
 res.render('transfer',{message:'Transfer Completed'});
-
 });
+
 app.get('/payment',(req,res)=>res.render('payment',{account:accounts.credit}));
-app.post('payment',(req,res)=>{
+
+app.post('/payment',(req,res)=>{
 accounts.credit.balance -=req.body.amount;
 accounts.credit.available += parseInt(req.body.amount, 10);
 const accountsJSON= JSON.stringify(accounts,null,4);
