d88e6711c08e5e79042a540c9c06ff6eb15a4cbc
vidyasagar <vidyasagar.ponnam@digiscloud.com>
2019-05-07 21:24:37 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app=express();
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({extended:true}));
 
 const accountData=fs.readFileSync(path.join(__dirname,'json','accounts.json'), 'utf8');
 const accounts=JSON.parse(accountData);
@@ -21,5 +22,22 @@ app.get('/savings',(req,res)=>res.render('account',{account:accounts.savings}));
 app.get('/checking',(req,res)=>res.render('account',{account:accounts.savings}));
 app.get('/credit',(req,res)=>res.render('account',{account:accounts.savings}));
 app.get('/profile',(req,res)=>res.render('profile',{user:users[0]}));
+app.get('/transfer',(req,res)=>res.render('transfer'));
+app.post('/transfer',(req,res)=>{
+accounts[req.body.from].balance=accounts[req.body.form].balance-req.body.amount;
+accounts[req.body.to].balance=parseInt(accounts[req.body.to].balance)+parseInt(req.body.amount,10);
+const accountsJSON= JSON.stringify(accounts,null,4);
+fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+res.render('transfer',{message:'Transfer Completed'});
+
+});
+app.get('/payment',(req,res)=>res.render('payment',{account:accounts.credit}));
+app.post('payment',(req,res)=>{
+accounts.credit.balance -=req.body.amount;
+accounts.credit.available += parseInt(req.body.amount, 10);
+const accountsJSON= JSON.stringify(accounts,null,4);
+fs.writeFileSync(path.join(__dirname,'json','accounts.json'),accountsJSON,'utf8');
+res.render('payment',{message:'Payment Successfull',account:accounts.credit});
+});
 app.listen(3000,()=>console.log("PS Project Running on port 3000!"));
 
